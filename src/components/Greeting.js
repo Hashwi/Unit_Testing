935ca216167e8586984a23caf9e9c45718fa27e7
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-14 17:11:25 +0200

    adding act to test case

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Greeting() {
-  return <div>
-    <h2> Hello World</h2>
-    <p>Nice to see you here!!</p>
-    </div>;
+  const [changedText, setChangedText] = useState(false);
+
+  const textChangeHandler = () => {
+    setChangedText(true);
+  };
+
+  return (
+    <div>
+      <h2> Hello World</h2>
+      {!changedText && <p>Nice to see you here!!</p>}
+      {changedText && <p>See You soon!!</p>}
+      <button onClick={textChangeHandler}>Change Text</button>
+    </div>
+  );
 }
 
 export default Greeting;
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe('Greeting Components', ()=> {
     test("renders Hello World as a text", () => {
@@ -13,6 +14,31 @@ describe('Greeting Components', ()=> {
         const helloWorldElement = screen.getByText('Hello World', {exact: false})
         expect(helloWorldElement).toBeInTheDocument()
         });
+
+        test("renders Nice to see you here if the button was NOT clicked", () => {
+            //Arrange
+            render(<Greeting/>)
+            
+            //Act
+            //... nothing
+            
+            // Assert
+            const outputElement = screen.getByText('Nice to see you here!!', {exact: false})
+            expect(outputElement).toBeInTheDocument()
+            });
+
+            test("renders 'See You soon' if the button was clicked", () => {
+                //Arrange
+                render(<Greeting/>)
+                
+                //Act
+                const buttonElement = screen.getByRole('button')
+                userEvent.click(buttonElement)
+                
+                // Assert
+                const outputElement = screen.getByText('See You soon!!', {exact: false})
+                expect(outputElement).toBeInTheDocument()
+                });
 
 })
 
